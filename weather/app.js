ab00a3b25e562f578410987f1b1dcc2a29e19a0b
Sergiogd112 <34186354+Sergiogd112@users.noreply.github.com>
2019-08-26 17:11:33 +0200

    updated index and app

--- weather/app.js
+++ weather/app.js
@@ -1,14 +1,30 @@
 window.addEventListener('load',()=>{
   let long;
   let lat;
+  let temperatureDescription = document.querySelector('.temperature-description')
+  let temperatureDegree = document.querySelector('.temperature-degree')
+
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position=>{
       console.log(position)
-      log=position.coords.longitude;
+      long=position.coords.longitude;
       lat=position.coords.latitude;
+      const proxy =`http://cors-anywhere.herokuapp.com/`
+      const api=`${proxy}https://api.darksky.net/forecast/b9dd7cda13788fa81e8c8062c4aedc28/${lat},${long}`
 
-    })
+      fetch(api)
+        .then(data =>{
+          return data.json();
+        })
+        .then(data=>{
+            console.log(data);
+            const {temperature,summary} = data.currently;
+        })
+
+
+    });
+
   }else {
     h1.textContent="hey this doesn't work"
   }
