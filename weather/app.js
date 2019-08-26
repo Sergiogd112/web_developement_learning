10a8f221873dc049b9bb0339d03605878514dc0a
Sergiogd112 <34186354+Sergiogd112@users.noreply.github.com>
2019-08-26 17:22:18 +0200

    icons

--- weather/app.js
+++ weather/app.js
@@ -1,13 +1,12 @@
 window.addEventListener('load',()=>{
   let long;
   let lat;
-  let temperatureDescription = document.querySelector('.temperature-description')
-  let temperatureDegree = document.querySelector('.temperature-degree')
-
+  let temperatureDescription = document.querySelector('.temperature-description');
+  let temperatureDegree = document.querySelector('.temperature-degree');
+  let locationTimezone = document.querySelector('.location-timezone');
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position=>{
-      console.log(position)
       long=position.coords.longitude;
       lat=position.coords.latitude;
       const proxy =`http://cors-anywhere.herokuapp.com/`
@@ -18,8 +17,12 @@ window.addEventListener('load',()=>{
           return data.json();
         })
         .then(data=>{
-            console.log(data);
             const {temperature,summary} = data.currently;
+            // Set DOM Elements from the API
+            temperatureDegree.textContent=temperature;
+            temperatureDescription.textContent=summary;
+            locationTimezone.textContent=data.timezone;
+
         })
 
 
